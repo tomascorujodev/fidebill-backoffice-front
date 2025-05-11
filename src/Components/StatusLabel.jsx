b00e1542597f8c644d60266bc1974835ce2a8e9b
tomascorujodev <tomascorujodev@gmail.com>
2025-05-11 12:44:45 -0300

    borrado de src

--- src/Components/StatusLabel.jsx
+++
@@ -1,9 +0,0 @@
-export default function StatusLabel({ status }) {
-  return (
-    <span
-      className={`badge ${status === 1 ? 'bg-success' : 'bg-danger'}`}
-    >
-      {status === 1 ? 'Activa' : 'Cancelada'}
-    </span>
-  );
-}
